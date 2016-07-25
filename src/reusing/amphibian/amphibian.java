5e78f187d40669f5018f96b88b67f575a456675c
afedorov <feodan@yandex.ru>
2016-07-25 17:35:39 +0300

    Inheritance syntax

---
+++ src/reusing/amphibian/amphibian.java
@@ -0,0 +1,13 @@
+package reusing.amphibian;
+
+public class Amphibian {
+    public void swim() {
+        System.out.println("amphibian swim");
+    }
+    public void eat() {
+        System.out.println("amphibian eat");
+    }
+    public void  repro() {
+        System.out.println("amphibian repro");
+    }
+}
